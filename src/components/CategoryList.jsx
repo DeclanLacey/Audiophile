ea226fcedf7e86b4706f4066e45d8e608632a2fd
DeclanLacey <declan.lacey@hotmail.com>
2024-01-08 13:33:06 -0600

    Styling and jsx for category list component

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,43 @@
+import {Link} from "react-router-dom"
+import "../styles/CategoryList.css"
+
+function CategoryList() {
+    return (
+        <section className="categoryList_container">
+            <div className="categoryList_headphones-container categoryList_category"> 
+                <img className="categoryList_img categoryList_img-one" src="/assets/shared/desktop/image-category-thumbnail-headphones.png" />
+                <h2 className="categoryList_name"> headphones</h2>
+                <div className="categoryList_link-container">
+                    <Link className="categoryList_link">shop </Link>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12" fill="none">
+                        <path d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
+                    </svg>
+                </div>
+            </div>
+
+            <div className="categoryList_speakers-container categoryList_category">
+                <img className="categoryList_img categoryList_img-two" src="/assets/shared/desktop/image-category-thumbnail-speakers.png" />
+                <h2 className="categoryList_name"> speakers</h2>
+                <div className="categoryList_link-container">
+                    <Link className="categoryList_link">shop </Link>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12" fill="none">
+                        <path d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
+                    </svg>
+                </div>
+            </div>
+
+            <div className="categoryList_earphones-container categoryList_category">
+                <img className="categoryList_img categoryList_img-three" src="/assets/shared/desktop/image-category-thumbnail-earphones.png" />
+                <h2 className="categoryList_name">earphones</h2>
+                <div className="categoryList_link-container">
+                    <Link className="categoryList_link">shop </Link>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12" fill="none">
+                        <path d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
+                    </svg>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CategoryList
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,13 @@
 import {Link} from "react-router-dom"
+import CategoryList from "./CategoryList"
 import "../styles/Homepage.css"
 
 
 function Homepage() {
     return (
         <div className="homepage_container">
-            <div className="homepage_main-img-container">
-                <img className="homepage_main-img" src="public/assets/home/mobile/image-header.jpg" />
+            <section className="homepage_main-img-container">
+                <img className="homepage_main-img" src="/assets/home/mobile/image-header.jpg" />
                 <div className="homepage_main-img-text-container">
                     <h2 className="homepage_main-img-subtitle"> new product </h2>
                     <h1 className="homepage_main-img-title"> XX99 mark II <br/> headphones </h1>
@@ -14,10 +15,10 @@ function Homepage() {
                     <Link to={""}> 
                         <button className="homepage_main-img-btn"> see product </button>
                     </Link>
-                    
                 </div>
-            </div>
+            </section>
             
+            <CategoryList />
         </div>
     )
 }
